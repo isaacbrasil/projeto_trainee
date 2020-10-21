377e8d0f43b9f9e68e1c6c8b286ce4729857f6ba
gilsonreisf <gilsonreisf@gmail.com>
2020-10-20 23:30:43 -0300

    src adicionado

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require ('express');
+
+const bodyParser = require('body-parser');
+
+const app = express();
+
+//Entende requisições em json que sao enviadas a api
+app.use(bodyParser.json());
+
+//Entende e decodifica parametros url passados a api
+app.use(bodyParser.urlencoded({ extended: false}));
+
+
+
+app.listen(3000);
